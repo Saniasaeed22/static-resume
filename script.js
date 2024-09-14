9b8c4d0d85da64ccb36d830b6399c1738c07f1d5
Sania Saeed <saniasaeed@633gmail.com>
2024-09-14 11:59:52 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.ts
+// Example of adding TypeScript functionality
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Resume page loaded');
+});
---
+++ script.ts
@@ -0,0 +1,6 @@
+// script.ts
+
+// Example of adding TypeScript functionality
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Resume page loaded');
+});
